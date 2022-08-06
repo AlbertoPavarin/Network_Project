1e6b9921026842c5b4fde0dfb5fced25a844c82f
AlbertoPavarin <albertopavarin@libero.it>
2022-08-07 01:37:50 +0200

    Aggiunta gruppi e async

--- network/network/api/consumers.py
+++ network/network/api/consumers.py
@@ -1,19 +1,42 @@
+from email import message
 import json
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.group_name = 'chat_' + self.room_name
 
-    def disconnect(self, code):
-        pass
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name,
+        )
 
-    def receive(self, text_data):
+        await self.accept()
+
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(
+            self.group_name,
+            self.channel_name
+        )
+
+    # Receive message from WebSocket
+    async def receive(self, text_data):
         data_json = json.loads(text_data)
         message = data_json['message']
 
-        self.send(text_data=json.dumps({ #json.dumps => obj to json str
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                'type': 'chat_mess',
+                'message': message
+            }
+        )
+
+    # Receive message from room group
+    async def chat_mess(self, event): # name of the method must be the same of the type
+        message = event['message']
+
+        await self.send(text_data=json.dumps({ #json.dumps => obj to json str
             'message': message
         }))
-
-        print(message)
--- network/network/network/settings.py
+++ network/network/network/settings.py
@@ -74,6 +74,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'network.wsgi.application'
 ASGI_APPLICATION = 'network.asgi.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
